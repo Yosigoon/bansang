46c6ec7125e6e72031146d7eccd70ff0d7bfdfe6
bit <bit@bitcamp>
2017-12-08 16:28:59 +0900

    ~search

---
+++ bansang/src/main/java/org/bansang/service/BansangServiceImpl.java
@@ -0,0 +1,43 @@
+package org.bansang.service;
+
+import java.util.List;
+
+import org.bansang.dto.Criteria;
+import org.bansang.dto.RecommendDTO;
+import org.bansang.dto.SearchCriteria;
+import org.bansang.mapper.BansangMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.java.Log;
+
+@Service
+@Log
+public class BansangServiceImpl implements BansangService {
+
+	@Autowired
+	BansangMapper bansangMapper;
+
+	@Override
+	public List<RecommendDTO> list(SearchCriteria cri) {
+		cri.setTotal(bansangMapper.getTotal(cri));
+		return bansangMapper.listPage(cri);
+	}
+
+	@Override
+	public RecommendDTO getModify(Long storeNumber) {
+		return bansangMapper.getModify(storeNumber);
+	}
+
+	@Override
+	public void delete(RecommendDTO dto) {
+		bansangMapper.delete(dto);
+		return;
+	}
+
+/*	@Override
+	public void modify(RecommendDTO dto) {
+		bansangMapper.modify(dto);
+		return;
+	}*/
+}
--- bansang/src/main/java/org/bansang/web/BansangController.java
+++ bansang/src/main/java/org/bansang/web/BansangController.java
@@ -1,8 +1,17 @@
 package org.bansang.web;
 
+import org.bansang.dto.Criteria;
+import org.bansang.dto.RecommendDTO;
+import org.bansang.dto.SearchCriteria;
+import org.bansang.service.BansangService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.java.Log;
 
@@ -11,18 +20,43 @@ import lombok.extern.java.Log;
 @Log
 public class BansangController {
 
+	@Autowired
+	BansangService bansangService;
+
 	@GetMapping("/profile")
 	public void getProfile() {
 		return;
 	}
-	
+
 	@GetMapping("/group")
 	public void getGroup() {
 		return;
 	}
-	
+
 	@GetMapping("/storeManagement")
-	public void getStore() {
+	public void getlist(Model model, @ModelAttribute("cri") SearchCriteria cri) {
+		model.addAttribute("list", bansangService.list(cri));
 		return;
 	}
+
+	@GetMapping("/storeModify")
+	public void GetModify(Model model, Long storeNumber, @ModelAttribute("cri") SearchCriteria cri) {
+		model.addAttribute("info", bansangService.getModify(storeNumber));
+		return;
+	}
+	
+	@PostMapping("/remove")
+	public String remove(RecommendDTO dto, RedirectAttributes rttr) {
+		bansangService.delete(dto);
+		rttr.addFlashAttribute("result", "delsuccess");
+		return "redirect:/bansang/storeManagement";
+	}
+
+	/*
+	 * @PostMapping("/storeModify") public String modify(RecommendDTO dto,
+	 * RedirectAttributes rttr) { rttr.addAttribute("storeNumber",
+	 * dto.getStoreNumber()); // url에 표시 bansangService.modify(dto);
+	 * rttr.addFlashAttribute("result", "modsuccess"); return
+	 * "redirect:/bansang/sotreManagement"; }
+	 */
 }
